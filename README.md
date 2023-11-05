2bb1fde751d329fc33621f7f135f367fcb37713e
Aaron Martin-Colby <amartincolby@gmail.com>
2023-11-04 20:18:16 -0400

    Updating code and readme

--- README.md
+++ README.md
@@ -1,14 +1,59 @@
 # Rust Quick Guide
 
-This will be a quick guide to learning Rust. I'm beginning work at the date of this commit (10/23/23) and hope to have it complete by the end of 2023.
+This will be a quick guide to learning Rust. I'm beginning work at the date of
+this commit (10/23/23) and hope to have it complete by the end of 2023.
 
-The Rust docs are very good but somewhat verbose. I like documentation that is simple enough to be a one-pager. I aim for [the famous Smalltalk postcard](https://richardeng.medium.com/syntax-on-a-post-card-cb6d85fabf88).
+I think that the best docs are short, sweet, specific, and do not elide the
+fundamentals undergirding the syntax. I tried to achieve this educational
+perfection with my [ReasonML Quick Guide](https://github.com/amartincolby/ReasonML-Quick-Guide),
+but ReasonML imploded with a community schism and thus caused me to abandon the
+language. I still have a fondness for ReasonML because it, and its connection to
+OCaml, were my introduction to the beautiful world of highly symbolic, academic
+languages, and I hope to communicate that to readers of this guide.
+
+The Rust docs are very good but somewhat verbose. I like documentation that is
+simple enough to be a one-pager. I aim for [the famous Smalltalk postcard](https://richardeng.medium.com/syntax-on-a-post-card-cb6d85fabf88).
+I did a previous quick guide for ReasonML, a syntax of OCaml, which profoundly
+inspired Rust's semantics.
 
 # Further Information
 - [The Official Learn Rust Online Book]([https://reasonml.org/](https://doc.rust-lang.org/book/title-page.html)) *The source of truth for learning Rust.*
 - [The Learn Rust Interactive Book](https://rust-book.cs.brown.edu/)
 
 # The Guide
+
+> “There are only two kinds of languages: bad programming languages and the
+> programming languages that nobody uses.”
+> 
+> ― Bjarne Stroustrup (paraphrased)
+
+I believe that we stand on the precipice of a great shift in software
+engineering. The last shift was the rise of object-oriented languages over forty
+years ago. Emerging as it did from the ashes of structured programming and the
+spaghetti code of _The Mythical Man Month_, OOP, as it is known, was the de
+rigeur paradigm just as computing exploded into the general consciousness. 
+Througout the 1980s and 1990s, the massive growth of the industry suppressed
+change, even as people on the proverbial edge started to realize that OOP had
+fundamental problems that were not easily soluble. These problems produced one
+of, if not the, greatest technical manuals in computing history: the legendary
+book _Design Patterns_ by the now-famous "Gang Of Four," a title they
+unfortunately share with Communist extremists from 1970s China. A critical
+analysis of _Design Patterns_ is outside the scope of this guide, suffice it to
+say that many analyses have shown the problems they solve are _linguistic_
+problems and not implementation problems. To wit, the patterns described are
+band-aids over a broken paradigm. _Design Patterns_ was published in 1994, but
+work on it started in 1990. We are over thirty years into an industry-wide quest
+to put lipstick on a pig.
+
+But beautiful change is in the air. Now is the autumn of the old ways, and I do not
+speak of AI. Indeed, AI, or at least the AI that is generating excitement at the
+current moment, is firmly rooted in the old ways: the ways of
+feature delivery at all costs, quality and symbolic integrity be damned. The new
+ways recognize that the fundamental tools at use must be built with the goal of
+symbolic integrity in mind. This means languages.
+
+Basically, it is time for the joke about good and bad programming languages to
+cease being true.
 
 The below text is valid Rust code. It is a copy of the code in the src directory.
 
---
+++ src/main.rs
@@ -0,0 +1,486 @@
+const _GREETING: &str = "Stay awhile. Stay foever.";
+
+/* Comment blocks start with slash-star,
+   and end with star-slash. */
+// Line comments begin with a double slash.
+
+/* The below is the main function which all Rust applications have. This quick
+    guide does not cover the structure of Rust applications. It focuses on the
+    syntax and semantics. For the purposes of this guide, it is sufficient to
+    know that Rust applications have a main function.
+
+    The syntax structures above the function are called attributes. They allow a
+    developer to specify how the function is the be handled by the compiler. In
+    these examples, three linting settings are being disabled. More attributes
+    will be covered later. */
+
+#[allow(unused_variables)]
+#[allow(unused_assignments)]
+#[allow(dead_code)]
+fn main() {
+
+    /*----------------------------------------------
+    * Variables, functions, and bindings
+    *-----------------------------------------------
+    */
+
+    /* Rust was inspired by the ML family of languages. As such, variables are
+    not, by default, variable; they are bindings. But unlike the more pure,
+    academic languages of OCaml and Haskell, Rust makes concessions to
+    real-world performance. Making values mutable increases performance. Thus,
+    Rust's compromise is to make values immutable by default. */
+
+    let immutable_value = 42;
+
+    // Note that the above binding is an evaluation, meaning the right-hand side
+    // is an expression. Expressions are code that return a value. The above is
+    // identical to:
+
+    let immutable_value = { 42 };
+
+    // This also illustrates a concept in Rust called variable shadowing. This
+    // is far from unique to Rust but allows an engineer to re-declare a
+    // variable and have the new value apply henceforth. Shadowing allows the
+    // type to be changed.
+
+    // Because the above is immutable, trying to assign a new value will cause a
+    // compiler failure.
+    // immutable_value = 2001;
+
+    // A mutable value is created with the `mut` modifier.
+    let mut mutable_value = 42;
+    mutable_value = 2001;
+
+    // Mutable values must still abide by the type of the original binding. The
+    // below would cause a compiler error.
+    // mutable_value = "One ring to rule them all.";
+
+    /*** Constants ***/
+
+    /* Rust's second method for declaring bindings is `const` for constant. If
+    coming from other languages, `const` sadly yet again means something
+    different in Rust. Like standard `let` bindings, `const` values cannot be
+    changed, but they also cannot be modified as mutable. Constants must have a
+    type annotation. Unlike `let`, constants can also be declared in any scope,
+    including the global scope. You can see a constant at the top of this file.
+    Finally, Constants must be the result of an expression that can be
+    determined at compile time. Constants cannot store a value that requires the
+    program to run to determine. */
+
+    // Constants are in all-caps.
+    const THE_ANSWER: i32 = 42;
+    const HALF_THE_ANSWER: i32 = THE_ANSWER / 2;
+    // const RANDOM_NUMBER: i32 = generate_random_int(); // Fails.
+
+    /* These behaviors make sense. Constants exist to provide a high-performance
+    method for storing data of known, fixed size for sharing across parts of the
+    application. */
+
+    /*** Type Inference ***/
+
+    // Rust does an excellent job of inferring types from values.
+
+    let x = 42; // 32-bit unsigned integer.
+    let y = 3.14; // 64-bit float.
+    let z = "Dinner for one"; // &str, a primitive string of fixed size.
+
+    // Functions will not infer parameter and return types.
+    // fn add_ints(a, b) { a + b } // This will fail to compile.
+    fn add_ints(a: i32, b: i32) -> i32 { a + b }
+
+    /*** Scope ***/
+
+    // Binding scopes are delineated by curly braces.
+    if true {
+        let val1 = "best of times";
+        println!("{}", val1)
+    };
+
+    let my_block = {
+        let val1 = "worst of times";
+        println!("{}", val1)
+    };
+
+    // Blocks are expressions, meaning that the last statement in the block is
+    // implicitly returned. Notice how a semicolon defines the termination of an
+    // evaluation block, meaning that no semicolon indicates the final return.
+    let my_block_value = { // Is set to 42.
+        let x = 20;
+        let y = 22;
+        x + y
+    };
+
+    /* The list of reserved words can be found here:
+    https://doc.rust-lang.org/reference/keywords.html
+
+    To see the unused variable warnings below, comment out
+    #[allow(unused_variables)] on line 15.
+
+    Prefixing a variable name with an underscore creates a casual variable.
+    These variables, if unused, will not trigger a compiler warning.
+    Unused variable warnings only apply to block scopes and will not be
+    raised on variables declared at the global or module level. 
+    
+    There are two kinds of unused variable. A suspicious unused variable is one
+    that has been bound with `let`. An innocuous variable is one that has not.*/
+
+    let block_scope = {
+        let variable = 42;           // This triggers a warning.
+        let _casual_variable = 2001; // This does not trigger a warning.
+        "Return string"
+    };
+
+    /*** References ***/
+
+    /* As a memory managed language, how data is passed around is a key part of
+    managing that memory. For those coming from TypeScript, this will seem like
+    a new concept, but it's not _that_ new.
+    
+    A reference is simply a pointer to something. If coming from TypeScript, you
+    are aware of the danger with passing an object or array to a function, since
+    if you mutate that object or array inside of that function, it gets changed
+    _outside_ of the function. That is because what you passed into the function
+    was a reference to the object or array, not the object or array itself.
+    
+    The only major difference in Rust is an engineer can explicitly reference
+    _any_ value, not just objects or arrays, by prepending an ampersand to the
+    variable name. */
+
+    let a_basic_number = 42; // type of i32
+    let another_number = a_basic_number; // another i32
+    let a_basic_reference = &a_basic_number; // type of &i32
+
+    /* In the above, a_basic_number is a 32-bit integer. When referencing
+    primitives, they are copied by default, so another_number is also a 32-bit
+    integer. But if an ampersand is prepended to the variable usage, it becomes
+    a reference that holds no value, but instead points to the location of
+    another value. The copy does not occur.
+    
+    There is more to be said about references, since Rust's management of values
+    is its party trick, but we will get to that in the section on ownership. */
+
+    // TODO: COver that. Copy the original values entirely to go back over what
+    // was previously discussed.
+
+    /*----------------------------------------------
+    * Primitive Types
+    *-----------------------------------------------
+    */
+
+    /*
+    &str
+    ints
+    floats
+    array
+    slice?
+     */
+
+    /* All of Rust's types have methods attached that can be accessed via dot
+    syntax or the double-colon operator. For example: */
+    
+    let a_number = 42;
+    let number_to_string = a_number.to_string();
+    let string_to_number = String::from(a_number);
+
+    /* The different syntax represents where the method exists. If using dot
+    syntax, the method exists on the instance and can potentially change. The
+    double-colon syntax means it is a method on the type itself and will never
+    change. A good analogy for those coming from other OO languages is to see
+    the double-colon as calling static methods. The different syntax helps
+    provide clarity as to where and how method calls work.
+    
+    The functionality seems duplicated, and for the use case here it is, but the
+    underlying process is very different. These will be discussed later.  */
+
+    // TODO: Cover this subject. Cover traits.
+
+    /*** > Integer ***/
+
+    let val1 = 1 + 1;          // i32 = 2
+    let val2 = 25 - 11;        // i32 = 11
+    let val3 = 5 * 2 * 3;      // i32 = 30
+    let val4 = 8 / 2;          // i32 = 4
+
+    // Integer division will round results
+    let val5 = 8 / 3;           // i32 = 2
+    let val6 = 8 / 5;           // i32 = 1
+
+    /* All integers in Rust are 32-bit signed integers by default, as you can
+    see above. Rust supports 8, 16, 32, 64, and 128-bit signed and unsigned
+    integers. No current architectures support 128-bit integers natively, so
+    usage of it can be dangerous for performance.
+    
+    Being a lower-level language, Rust also supports explicit support for
+    per-architecture typing of both signed and unsigned integers. This means
+    that using these types on a 64-bit Windows system will result in 64-bit
+    integers. I am writing this on a Samsung Windows laptop, meaning these
+    integers below are 64-bit on my system. */
+
+    let arch_s_int: isize = 42;
+    let arch_u_int: usize = 42;
+
+    /*** > Float ***/
+
+    let float1 = 1.1 + 1.5;     // float = 2.6
+    let float2 = 18.0 - 24.5;   // float = -6.5
+    let float3 = 2.5 * 2.0;     // float = 5.0
+    let float4 = 16.0 / 4.0;    // float = 4.0
+
+    // Floats and integers can be formatted with underscores. These underscores
+    // are purely visual.
+    let formatted_int = 12_34_56;       // i32 = 123456
+    let formatted_float = 12_34_56.0;   // f64 = 123456.0
+    
+
+    /*** > String ***/
+
+    /* Strings in Rust are not true primitives in the sense that a primitive is
+    a thing of known, fixed size. They are like C strings in that they are best
+    described as an array of characters, with each character being the true
+    primitive.
+    
+    The fundamental type, to wit the type that is part of the language itself,
+    is `str`, which is called a string slice. The `String` type is actually part
+    of the standard library and is best described as a wrapper around `str` that
+    provides helpful functionality.
+    
+    You can think of a string slice  */
+
+    // Use double quotes for strings.
+    let greeting = "Hello world!"; // type of &str
+    let another_greeting = greeting + " Glad to be here.";
+
+    // Strings can span multiple lines. When compiled, new line characters are generated.
+    let a_longer_greeting = "Look at me,
+    I'm a
+    multi-line string";
+
+    // Strings 
+    let helloWorld = {j|hello, $(world)|j};
+
+
+
+    // Concatenate strings with `++`.
+    let name = "John " ++ "Wayne";
+    let emailSubject = "Hi " ++ name ++ ", you're a valued customer";
+
+
+    /*** > Char ***/
+
+    /* Use single quotes for a char. Chars compile to an integer between 0 and 255
+        and thus do not support Unicode or UTF-8. */
+
+    let lastLetter = 'z';
+
+    
+
+    /*----------------------------------------------
+    * Structs, Tuples, & Enums
+    *-----------------------------------------------
+    */
+
+    /* As I mentioned, Rust is heavily inspired by OCaml. It tries to bridge the
+    conceptual gap between academic and highly symbolic languages like ML and
+    hardware-oriented languages like C. This means that the linguistic
+    traditions of both languages sit sometimes uneasily next to one another.
+    
+    An excellent example of this is the distinction between "types", as
+    illustrated above, and structs, tuples, traits, interfaces, and
+    implementations. In C, these are different things because they represent
+    different things in memory. In OCaml, there is no significant difference;
+    they are all just types. A type is simply a description of the ways with
+    which a thing can be interacted, and anything that fulfills that is simply
+    declared with `type`.
+    
+    Rust documentation sometimes conflates these two traditions, calling things
+    types in one place, then calling them structs or tuples in another. I will
+    try to solidify my term usage going forward. Structs, tuples, enums, and
+    traits are collectively called types because a type necessarily does not
+    define *how* something is done, only *what* is done. As such, even though
+    implementations add to the shape of a struct and thus alter its type
+    signature, they are not truly an extension of the type per se. The type
+    extension is incidental to the implementation. */
+
+    /*** Structs ***/
+    
+    /* A struct is a grouping of entities under a single name and usually
+    sitting together in memory. The terminology and symblic nature are from C,
+    which itself took the concept from ALGOL.
+
+    From the developer perspective, a struct is like a blueprint that is built
+    when the struct is instantiated. For developers coming from TypeScript, it
+    may look like a type or interface, but there are differences which will be
+    discussed.
+    
+    The first, and most notable difference, is that structs can only contain
+    data primitives. This is because, as mentioned, an instantiated struct
+    represents data sitting together in memory. As such, the data must be of
+    known, fixed size at the moment of instantiation. */
+
+    // Idiomatic Rust uses CamelCase for struct declarations.
+    struct UserData {
+        id: String,
+        name: String,
+        display_name: String,
+        bank_balance: f64,
+    }
+
+    // Unlike TypeScript, but like C, a struct is not simply annotated on an
+    // object. The struct is instantiated explicitly.
+
+    let mut a_user = UserData {
+        id: String::from("abc123"),
+        name: String::from("Duncan Idaho"),
+        display_name: String::from("The Duncanator"),
+        bank_balance: 3.14,
+    };
+
+    /* Note how the above UserData instance is tagged as mutable. This allows
+    changes to the internal values *and* the binding, but another instance of
+    the same struct must be used. Individual fields of a struct cannot be
+    independently tagged as mutable. */
+
+    /* ReasonML makes little distinction between primitive types and what could be
+    called user-defined types or structs. They are simply "things" represented
+    by symbols.
+
+    The below type is an abstract type, meaning that the symbol has no structure
+    attached to it. Any program using this type does not need to know its
+    structure just so long as usage of the type is consistent. Basically,
+    programs can use symbols without knowing what they mean. */
+
+    type kwyjibo;
+
+    /*** Tuples ***/
+
+    /* The Rust documentation calls tuples a form of struct which I disagree
+    with. They are indeed the same thing, but I think the unifying term "type"
+    is more appropriate than struct and tuple struct.
+    
+    But I digress.
+    
+    */
+    
+    /*
+    This is sounding very object-orienty, but
+    don't worry. Rust is not beholden to the mistakes of past languages. */
+
+    /*** Struct Methods ***/
+
+    /* As mentioned, structs are just grouped data. They are not like classic
+    "objects" in the sense of having behavior and data. To add behaviors, aka
+    methods, to a struct, it must be implemented. */
+
+    struct Square {
+        width: f64,
+        height: f64,
+    }
+
+    impl Square {
+        fn area(&self) -> f64 {
+            self.height * self.width
+        }
+    }
+
+    /*** Traits ***/
+
+    /*** Tuples ***/
+
+    /* Rust refers to tuples as a form of struct, which makes sense. If a struct
+    is a set of primitive values in a shared space, sitting together in memory,
+    then a classic tuple fits the bill. Just as with traditional structs */
+
+    
+    /*----------------------------------------------
+    * Basic operators
+    *-----------------------------------------------
+    */
+
+    /* The reason for putting basic operators so late into this tutorial is
+    because they are somewhat supercharged in Rust. Instead of relying on fixed
+    operators, custom evaluators can be written, thus allowing engineers to
+    decide how operators such as > or == function. */
+
+    /*** > Boolean ***/
+
+    // A boolean can be either true or false.
+    let is_learning = true;
+
+    let logical_and = true && false; // - : bool = false; 
+    let logical_or = true || false;  // - : bool = true;
+    let logical_not = !true;         // - : bool = false;
+
+    'a' > 'b'; // - bool : false
+    5 < 42;    // - bool : true
+
+    /* Equality */
+
+    /* In the below example, we want to use the equality operator on a struct.
+    Rust comes with operator capabilities built in, but for custom structs these
+    capabilities are not attached by default. The struct needs to be annotated
+    with a #[derive] attribute that will generate the specified trait and attach
+    it to the struct. Below, the PartialEq trait will be attached, thus allowing
+    three equality operations. If the attribute is commented out, authors_1, 2,
+    and 3 will fail to compile. */
+
+    #[derive(PartialEq)]
+    struct Author {
+        name : String,
+        age  : i32
+    };
+
+    let author1 = Author {
+        name : String::from("Charles Dickens"),
+        age  : 58
+    };
+
+    let author2 = Author {
+        name : String::from("Charles Dickens"),
+        age  : 58
+    };
+
+    let author3 = Author {
+        name : String::from("Victor Hugo"),
+        age  : 83
+    };
+
+    let authors_1 = author1 == author2; // - : bool = true
+    let authors_2 = author1 == author3; // - : bool = false
+    let authors_3 = author1 != author3; // - : bool = true
+
+    /* Any attempt at using greater-than or less-than vis-à-vis structures will
+        trigger a structural comparison. The comparison will return a boolean as 
+        soon as a difference is discovered and will not continue to do a complete
+        comparison. */
+
+    let bigObj = [10, 10000000, 10000000];
+    let smallObj = [11, 1, 1];
+
+    bigObj > smallObj; // - : bool = false
+
+    /* Because 10 and 11 are different, and 10 is smaller than 11, false is returned
+        even though the next two values are much larger. */
+
+    /* Referential, or physical, equality, using the triple-glyph operator, compares
+        the identity of each entity. */
+
+    author1 === author2; // - : bool = false
+    author1 === author1; // - : bool = true
+
+
+    /* Comparing Values */
+
+    // The equality operators work differently for values instead of structures.
+    // Both `==` and `===` will become strict equality `===` when compiled to JavaScript.
+    // Attempting to compare two different types will cause a compile error.
+
+    let myString1 = "A world without string is chaos.";
+    let myString2 = "A world without string is chaos.";
+
+    "A string" === "A string"; // - : bool = true
+    "A string" == "A string";  // - : bool = true
+    42 === 42;                 // - : bool = true
+    42 == 42;                  // - : bool = true
+    // 42 === "A string"       // Error
+
+}
